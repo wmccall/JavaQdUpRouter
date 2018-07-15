e9deadacc4a168cd99c6ca58e8262ae23f7d9a72
Will McCall <dev@willmccall.com>
2018-07-14 22:49:08 -0400

    Change order of yml

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 cd /home/ec2-user/raw
 sudo yum -y install ant
-mkdir dep/LIBEVENT
-cd dep/LIBEVENT
+mkdir dep
+cd dep
+mkdir LIBEVENT
+cd LIBEVENT
 curl -OL https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
 sudo tar -xvzf libevent-2.0.21-stable.tar.gz
 cd libevent-2.0.21-stable
 sudo ./configure --prefix=/usr/local
 sudo make
 sudo make install
-cd ../../..
-mkdir dep/TMUX
-cd dep/TMUX
+cd ../..
+mkdir TMUX
+cd TMUX
 curl -OL https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
 sudo tar -xvzf tmux-2.0.tar.gz
 cd tmux-2.0
