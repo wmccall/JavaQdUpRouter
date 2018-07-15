1285e1810c3fe682dd28972125cc937ffa59e316
Will McCall <dev@willmccall.com>
2018-07-14 22:41:15 -0400

    Add more dependency installers

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 cd /home/ec2-user/raw
-mkdir LIBEVENT
-cd LIBEVENT
+sudo yum -y install ant
+mkdir dep/LIBEVENT
+cd dep/LIBEVENT
 curl -OL https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
 sudo tar -xvzf libevent-2.0.21-stable.tar.gz
 cd libevent-2.0.21-stable
 sudo ./configure --prefix=/usr/local
 sudo make
 sudo make install
-cd ../..
-mkdir TMUX
-cd TMUX
+cd ../../..
+mkdir dep/TMUX
+cd dep/TMUX
 curl -OL https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
 sudo tar -xvzf tmux-2.0.tar.gz
 cd tmux-2.0
 sudo LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
 sudo make
 sudo make install
-cd ../..
+cd ../../..
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user/raw
-sudo yum -y install ant
 ant clean compile jar
