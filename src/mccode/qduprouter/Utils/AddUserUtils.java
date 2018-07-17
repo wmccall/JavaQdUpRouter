f7b78ec74787bbefc687fd0f076dea9484a54c83
Will McCall <dev@willmccall.com>
2018-07-16 22:02:26 -0400

    Fix index out of bounds error

--- src/mccode/qduprouter/Utils/AddUserUtils.java
+++ src/mccode/qduprouter/Utils/AddUserUtils.java
@@ -12,7 +12,7 @@ public class AddUserUtils {
     public static String generateRandomKey(){
         String key = "";
         for(int i = 0; i < 6; i++){
-            key = key + alphanumeric[r.nextInt()*34];
+            key += "" + alphanumeric[r.nextInt()*33];
         }
         return key;
     }
